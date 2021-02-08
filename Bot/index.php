a8b553a37e4c0e70eea1966f0a9fbdac57d740be
Lusifer <70184956+lusifernoob@users.noreply.github.com>
2021-02-08 08:10:36 +0530

    Update index.php

--- Bot/index.php
+++ Bot/index.php
@@ -75,8 +75,8 @@ Also add me to your group and make me admin. I'll reply to every photo😌.***",
 'parse_mode'=>"MarkDown",
 'reply_markup' =>  json_encode([
 'inline_keyboard' => [
-[['text' => "SUPPORT",'url' => "https://telegram.me/induschats"],['text' => "CREATOR", 'url' => "https://telegram.me/benchamxd"]],
-[['text' => "ADD ME", 'url' => "https://telegram.me/$BOT_USERNAME?startgroup=False"],['text' => "Repo", 'url' => "https://github.com/Benchamxd/Ai-Bot"]], 
+[['text' => "SUPPORT",'url' => "https://telegram.me/OMG_info"],['text' => "CREATOR", 'url' => "https://telegram.me/shado_hackers"]],
+[['text' => "ADD ME", 'url' => "https://telegram.me/$BOT_USERNAME?startgroup=False"],['text' => "Repo", 'url' => "error 404 file not found"]], 
 ]])
 ]);
 }
